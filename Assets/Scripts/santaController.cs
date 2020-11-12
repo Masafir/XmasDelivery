fa599298a8e11d8079e7621e715e1d8a2e2b161f
Masafir <amiralmonkey@gmail.com>
2020-11-12 16:23:27 +0100

    character animation for idle and walk

---
+++ Assets/Scripts/santaController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class santaController : MonoBehaviour
+{
+    // Animations du perso
+    Animation animations;
+
+ 
+
+    // Vitesse de déplacement
+    public float walkSpeed;
+    public float runSpeed;
+    public float turnSpeed;
+
+
+
+
+    // Inputs
+    public string inputFront;
+    public string inputBack;
+    public string inputLeft;
+    public string inputRight;
+    //public string space;
+
+
+
+
+    //public Vector3 jumpSpeed;
+    CapsuleCollider playerCollider;
+
+
+
+
+    void Start()
+    {
+        animations = gameObject.GetComponent<Animation>();
+        playerCollider = gameObject.GetComponent<CapsuleCollider>();
+    }
+
+
+
+    bool IsGrounded()
+    {
+        return Physics.CheckCapsule(playerCollider.bounds.center, new Vector3(playerCollider.bounds.center.x,
+            playerCollider.bounds.min.y - 0.1f, playerCollider.bounds.center.z), 0.08f);
+    }
+
+
+
+    void Update()
+    {
+
+
+
+        // si on avance
+        if (Input.GetKey(inputFront) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(0, 0, walkSpeed * Time.deltaTime);
+            animations.Play("Walk");
+        }
+
+
+
+        // Si on sprint
+        if (Input.GetKey(inputFront) && Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(0, 0, runSpeed * Time.deltaTime);
+            animations.Play("run");
+        }
+
+
+
+        // si on recule
+        if (Input.GetKey(inputBack))
+        {
+            transform.Translate(0, 0, -(walkSpeed / 2) * Time.deltaTime);
+            animations.Play("Walk");
+
+        }
+
+
+
+        // rotation à gauche 
+        if (Input.GetKey(inputLeft))
+        {
+            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+        }
+
+
+
+        // rotation à droite 
+        if (Input.GetKey(inputRight))
+        {
+            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+        }
+
+
+
+        // Si on avance pas et que on recule pas non plus
+        if (!Input.GetKey(inputFront) && !Input.GetKey(inputBack))
+        {
+            animations.Play("Idle");
+        }
+
+
+
+        // Si on saute
+        /*if (Input.GetKey(space) && IsGrounded())
+        {
+            // Préparation du saut (Nécessaire en C#)
+            Vector3 v = gameObject.GetComponent<Rigidbody>().velocity;
+            v.y = jumpSpeed.y;
+
+
+
+            // Saut
+            gameObject.GetComponent<Rigidbody>().velocity = jumpSpeed;
+        }*/
+    }
+}
