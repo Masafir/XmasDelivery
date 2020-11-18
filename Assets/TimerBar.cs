7f421075ac84d4748b950f85fe96a48fc10590ed
Tifay <37075155+Tifay@users.noreply.github.com>
2020-11-18 11:43:23 +0100

    add countdown timer and slice bar

---
+++ Assets/TimerBar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.UI;
+ 
+ public class TimerBar : MonoBehaviour
+ {
+     public static bool allowInputs;
+ 
+     public Slider timerBar;
+     private bool countDown = true;
+ 
+     public float countDownTime = 8;
+     public float refillTime = 10;
+ 
+     private void Start()
+     {
+         timerBar.maxValue = refillTime;
+     }
+ 
+     private void Update()
+     {
+         if (timerBar.maxValue != refillTime)
+             timerBar.maxValue = refillTime;
+ 
+             if (countDown) 
+             timerBar.value -= Time.deltaTime / countDownTime * refillTime;
+         else
+             timerBar.value += Time.deltaTime;
+ 
+      
+         if (timerBar.value <= 0)
+         {
+             countDown = false;
+             allowInputs = false;
+         }
+         else if (timerBar.value >= refillTime)
+         {
+             countDown = true;
+             allowInputs = true;
+         }
+     }
+ }
